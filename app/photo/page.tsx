97ee3de2e6f1b62ab2e2afc8a6883811c6867d84
Junhao Qu <869142649qq@gmail.com>
2025-09-29 22:41:45 -0700

    add selection

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -15,12 +15,28 @@ const buildCloudinaryImageUrl = (publicId: string) => {
 };
 
 const PhotoPage = () => {
+  const bgImages = [
+    "firework1_cshymx",
+    "Star_trail_small_r3zut3",
+  ];
+
+  const [currentBgIndex, setCurrentBgIndex] = React.useState(0);
+
+  const handlePrev = () => {
+    setCurrentBgIndex((prevIndex) => (prevIndex - 1 + bgImages.length) % bgImages.length);
+  };
+
+  const handleNext = () => {
+    setCurrentBgIndex((prevIndex) => (prevIndex + 1) % bgImages.length);
+  };
+
+
   useEffect(() => {
     const timeline = gsap.timeline({
       scrollTrigger: {
         trigger: ".photo-wrapper",
         start: "top top",
-        end: "+=200%",
+        end: "+=150%",
         pin: true,
         scrub: true,
       }
@@ -29,14 +45,14 @@ const PhotoPage = () => {
     timeline
       .to(".photo-image", {
         scale: 3,
-        z: 360,
+        z: 400,
         transformOrigin: "center center",
         ease: "power1.inOut"
       })
       .to(
         ".section.hero",
         {
-          scale: 1,
+          scale: 1.1,
           transformOrigin: "center center",
           ease: "power1.inOut"
         },
@@ -49,7 +65,8 @@ const PhotoPage = () => {
   }, []);
 
   const heroStyle = {
-    backgroundImage: `url(${buildCloudinaryImageUrl("firework1_cshymx")})`,
+    backgroundImage: `url(${buildCloudinaryImageUrl(bgImages[currentBgIndex])})`,
+    transition: 'background-image 0.5s ease-in-out',
   };
 
   return (
@@ -60,6 +77,17 @@ const PhotoPage = () => {
       <div className="image-container">
         <img src={buildCloudinaryImageUrl("window_k8hdtc")} alt="image" className="photo-image" />
       </div>
+
+      <button onClick={handlePrev} className="arrow-button left-arrow">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.75 19.5L8.25 12L15.75 4.5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </button>
+      <button onClick={handleNext} className="arrow-button right-arrow">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M8.25 4.5L15.75 12L8.25 19.5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </button>
     </div>
   );
 };
